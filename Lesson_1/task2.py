51d66904b8b5d7723bd125e1a5a081f90ae4356a
YemelyanovYaroslav <emelanovaroslav3@gmail.com>
2022-11-12 15:24:34 +0200

    Done Lesson 1 Assignment 2

---
+++ Lesson_1/task2.py
@@ -0,0 +1,23 @@
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+operator = input("Select an operation: ")
+
+if operator == '+':
+    {
+        print(num_1 + num_2)
+    }
+
+if operator == '-':
+    {
+        print(num_1 - num_2)
+    }
+
+if operator == '*':
+    {
+        print(num_1 * num_2)
+    }
+
+if operator == '/':
+    {
+        print(num_1 / num_2)
+    }
